b2d04e096555a4fb2b2e1c2b5e8af1af7cf8bc0f
Hil-dred <hildredadebayo@gmail.com>
2022-08-10 20:30:31 +0100

    Edit shell script

--- deploy-to-k8s.sh
+++ deploy-to-k8s.sh
@@ -5,7 +5,7 @@ docker build -t client-go:kubectl .
 # 'helm install' for a new deployment
 # 'helm upgrade' for a helm revision
 
-helm upgrade clientgo-helm clientgo-helm  -f ./clientgo-helm/values.yaml
+helm install clientgo-helm clientgo-helm  -f ./clientgo-helm/values.yaml
 
 kubectl config set-context --current --namespace=a
 
